f709e4a0b51c53850010184b4d2e943524970738
webyom <webyom@gmail.com>
2021-05-06 20:50:10 +0800

    feat: lib styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 exports.scss = function() {
   return gulp
     .src('src/**/*.scss')
+    .pipe(gulp.dest('es'))
+    .pipe(gulp.dest('lib'));
+};
+
+exports.lib = function() {
+  const plugins = [
+    autoprefixer(),
+    cssnano(),
+  ];
+  return gulp
+    .src('src/styles/lib.scss', { base: 'src/styles' })
+    .pipe(sass())
+    .pipe(postcss(plugins))
     .pipe(gulp.dest('es'))
     .pipe(gulp.dest('lib'));
 };
